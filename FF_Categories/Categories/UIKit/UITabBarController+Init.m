a4fae1ad9eabd7c4e7fbe55d5b4231a630c35193
FanXiaobin <527256662@qq.com>
2018-03-27 17:42:56 +0800

    添加快速搭建UI框架的类目
    添加快速搭建UI框架的类目

---
+++ FF_Categories/Categories/UIKit/UITabBarController+Init.m
@@ -0,0 +1,90 @@
+//
+//  UITabBarController+Init.m
+//  FF_CustomBarAppearence
+//
+//  Created by fanxiaobin on 2017/9/27.
+//  Copyright © 2017年 fanxiaobin. All rights reserved.
+//
+
+#import "UITabBarController+Init.h"
+//#import "BaseNavigationController.h"
+
+@implementation UITabBarController (Init)
+
+-(void)customNavigationBarAppearence{
+   
+    [UINavigationBar appearance].barTintColor = kMainColor;
+    [UINavigationBar appearance].tintColor = kWhiteColor;
+    [[UINavigationBar appearance] setTitleTextAttributes:@{NSFontAttributeName : [UIFont systemFontOfSize:18], NSForegroundColorAttributeName : kWhiteColor}];
+    [UINavigationBar appearance].translucent = NO;
+    [UINavigationBar appearance].shadowImage = [UIImage new];
+    //[[UINavigationBar appearance] setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    [UINavigationBar appearance].backIndicatorImage = [UIImage imageNamed:@"back_b"];
+    [UINavigationBar appearance].backIndicatorTransitionMaskImage = [UIImage imageNamed:@"back_b"];
+   
+    ///TabBar显示设置
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor]} forState:UIControlStateNormal];
+    UIColor *titleHighlightedColor = kMainColor;
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : titleHighlightedColor} forState:UIControlStateSelected];
+}
+
+- (UINavigationController *)setupViewController:(NSString *)viewControllerClassName title:(NSString *)title image:(NSString *)imageName selectedImage:(NSString *)selImageName{
+    
+    Class cls = NSClassFromString(viewControllerClassName);
+   
+    UIViewController *vc = [[cls alloc] init];
+    UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+    
+    UIImage *normalImage = [[UIImage imageNamed:imageName] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    UIImage *selImage = [[UIImage imageNamed:selImageName] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    UITabBarItem *item = [[UITabBarItem alloc] initWithTitle:title image:normalImage selectedImage:selImage];
+    item.titlePositionAdjustment = UIOffsetMake(0, -3);
+    nc.tabBarItem = item;
+    
+    return nc;
+}
+
+-(void)setupTabBarControllerWithViewControllerNames:(NSArray *)viewControllerNames titles:(NSArray *)titles normalImages:(NSArray *)normalImages selectedImages:(NSArray *)selectedImages{
+    
+    ///TabBar显示设置
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor]} forState:UIControlStateNormal];
+    UIColor *titleHighlightedColor = kMainColor;
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : titleHighlightedColor} forState:UIControlStateSelected];
+    
+    NSMutableArray *viewControllers = [NSMutableArray array];
+    for (int i = 0; i < viewControllerNames.count; i++) {
+        NSString *viewControllerClassName = viewControllerNames[i];
+        UINavigationController *nc = [self setupViewController:viewControllerClassName title:titles[i] image:normalImages[i] selectedImage:selectedImages[i]];
+        [viewControllers addObject:nc];
+    }
+    self.viewControllers = viewControllers;
+    
+}
+
+-(void)setupTabBarControllerWithViewControllers:(NSArray *)viewControllers titles:(NSArray *)titles normalImages:(NSArray *)normalImages selectedImages:(NSArray *)selectedImages{
+    
+    ///TabBar显示设置
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor]} forState:UIControlStateNormal];
+    UIColor *titleHighlightedColor = kRedColor;
+    [[UITabBarItem appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName : titleHighlightedColor} forState:UIControlStateSelected];
+    
+    NSMutableArray *aviewControllers = [NSMutableArray array];
+    for (int i = 0; i < viewControllers.count; i++) {
+        UIViewController *vc = viewControllers[i];
+        UINavigationController *nc = [[UINavigationController alloc] initWithRootViewController:vc];
+        
+        UIImage *normalImage = [[UIImage imageNamed:normalImages[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        UIImage *selImage = [[UIImage imageNamed:selectedImages[i]] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        
+        UITabBarItem *item = [[UITabBarItem alloc] initWithTitle:titles[i] image:normalImage selectedImage:selImage];
+        item.titlePositionAdjustment = UIOffsetMake(0, -3);
+        nc.tabBarItem = item;
+        
+        [aviewControllers addObject:nc];
+    }
+    self.viewControllers = aviewControllers;
+    
+}
+
+@end
